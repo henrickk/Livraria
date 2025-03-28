0669ad82299866c5e87efdf89c54e66078daf308
Henrick Adrian <henrick.adrian4@gmail.com>
2025-03-28 19:58:22 -0300

    Add project files.

---
+++ Models/Livros.cs
@@ -0,0 +1,12 @@
+﻿namespace Livraria.Models
+{
+    public class Livros
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Autor { get; set; }
+        public string? AnoPublicacao { get; set; }
+        public string? Genero { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
