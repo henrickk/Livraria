6509d6d06e71259e15510fe405aa4d3f1f9fa867
Henrick Adrian <henrick.adrian4@gmail.com>
2025-03-29 20:50:36 -0300

    Criando metodos http

---
+++ Controllers/LivrosController.cs
@@ -0,0 +1,46 @@
+﻿using Livraria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Livraria.Controllers
+{
+    [ApiController]
+    [Route("api/livros")]
+    public class LivrosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult MostrarTodosOsLivros(Livros livoros)
+        {
+            return Ok(livoros);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult BuscaLivro(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarLivro(Livros livro)
+        {
+            return CreatedAtAction("Get", new { id = livro.Id}, livro);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult AtualizarLivro(int id, Livros livro)
+        {
+            if (id != livro.Id)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeletarLivro(int id)
+        {
+            return NoContent();
+        }
+    }
+}
--- Models/Livros.cs
+++ Models/Livros.cs
@@ -5,8 +5,10 @@
         public int Id { get; set; }
         public string? Titulo { get; set; }
         public string? Autor { get; set; }
-        public string? AnoPublicacao { get; set; }
-        public string? Genero { get; set; }
+
+        
+        public DateOnly AnoPublicacao { get; set; }
+        public List<string>? Genero { get; set; }
         public decimal Preco { get; set; }
     }
 }
