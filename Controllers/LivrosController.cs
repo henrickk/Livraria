dc54257c1a1c26dbd88b7c4395606e8734a5fd3e
Henrick Adrian <henrick.adrian4@gmail.com>
2025-03-30 22:03:47 -0300

    Criando uma lista fixa (sem adicionar dinamicamente) como referência.

--- Controllers/LivrosController.cs
+++ Controllers/LivrosController.cs
@@ -9,24 +9,61 @@ namespace Livraria.Controllers
     public class LivrosController : ControllerBase
     {
         [HttpGet("MostrarTodosOsLivros")]
-        public IActionResult MostrarTodosOsLivros(Livros livoros)
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status200OK)]
+        public IActionResult MostrarTodosOsLivros()
         {
-            return Ok(livoros);
+            var livros = new List<Livros>
+            {
+                new Livros
+                {
+                    Id = 1,
+                    Titulo = "Harry Potter e a Pedra Filosofal",
+                    Autor = "J. K. Rowling",
+                    AnoPublicacao = new DateOnly(1997, 6, 26),
+                    Genero = new List<string> { "Romance", "Literatura fantástica", "Literatura infantil", "Alta fantasia" },
+                    Preco = 45.38M
+                },
+                new Livros
+                {
+                    Id = 2,
+                    Titulo = "Harry Potter e a Câmara Secreta",
+                    Autor = "J. K. Rowling",
+                    AnoPublicacao = new DateOnly(1998, 7, 2),
+                    Genero = new List<string> { "Romance", "Literatura fantástica", "Bildungsroman", "Alta fantasia", " Ficção de aventura" },
+                    Preco = 55.17M
+                },
+                new Livros
+                {
+                    Id = 3,
+                    Titulo = "Harry Potter e o Prisioneiro de Azkaban",
+                    Autor = "J. K. Rowling",
+                    AnoPublicacao = new DateOnly(1999, 7, 8),
+                    Genero = new List<string> { "Romance", "Literatura fantástica" },
+                    Preco = 38.94M
+                },
+            };
+            return Ok(livros);
         }
 
         [HttpGet("BuscaLivro/{id:int}")]
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult BuscaLivro(int id)
         {
-            return Ok();
+            return Ok(new Livros());
         }
 
         [HttpPost("AdicionarLivro")]
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult AdicionarLivro(Livros livro)
         {
-            return CreatedAtAction("Get", new { id = livro.Id}, livro);
+            return CreatedAtAction("BuscaLivro", new { id = livro.Id }, livro);
         }
 
         [HttpPut("AtualizarLivro/{id:int}")]
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult AtualizarLivro(int id, Livros livro)
         {
             if (id != livro.Id)
@@ -38,6 +75,8 @@ namespace Livraria.Controllers
         }
 
         [HttpDelete("DeletarLivro/{id:int}")]
+        [ProducesResponseType(typeof(Livros), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult DeletarLivro(int id)
         {
             return NoContent();
--- Models/Livros.cs
+++ Models/Livros.cs
@@ -1,12 +1,14 @@
-﻿namespace Livraria.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Livraria.Models
 {
     public class Livros
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "o campo {0} é obrigatório")]
         public string? Titulo { get; set; }
         public string? Autor { get; set; }
-
-        
         public DateOnly AnoPublicacao { get; set; }
         public List<string>? Genero { get; set; }
         public decimal Preco { get; set; }
